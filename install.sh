445163b8ad04fbc780b581cb44dcb520f4eca47e
Joy Yeh <joyyeh.tw@gmail.com>
2022-05-09 13:28:56 +0900

    Fix install dld path

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
 dest=/usr/local/bin/aws_mfa_me
-curl -s https://raw.githubusercontent.com/joy13975/aws_mfa_me/master/aws_mfa_me > \
+curl -s https://raw.githubusercontent.com/joy13975/aws_mfa_me/master/bin/aws_mfa_me > \
     $dest
 chmod +x $dest
